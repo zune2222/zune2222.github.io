c463fcfd365ea4024bd3326c2b6f8b1b437ce9c7
zune2222 <zun_e@kakao.com>
2023-06-12 14:30:44 +0000

    애니메이션 완성 ! !

--- app/page.js
+++ app/page.js
@@ -1,13 +1,38 @@
-import Image from 'next/image'
+'use client';
+import Image from 'next/image';
+import { useState } from 'react';
+import { useSpring, animated } from '@react-spring/web';
 export default function Home() {
-  return (
-    <div className='flex flex-col p-3 min-h-screen justify-start items-center'>
-		<div className='flex-initial rounded-lg shadow-xl w-full h-96 p-3 mb-3  bg-gradient-to-r from-cyan-500 to-blue-500'>
-			asdf
+	const [currentMode, setCurrentMode] = useState(false);
+	const springs = useSpring(
+		currentMode === false
+			? {
+					config: {
+						tension: 300,
+						mass: 0.1,
+					},
+					width: '4rem',
+					height: '4rem',
+			  }
+			: {
+					config: {
+						tension: 250,
+						mass: 2,
+					},
+					width: '32rem',
+					height: '12rem',
+			  }
+	);
+	const handleClick = () => {
+		setCurrentMode(!currentMode);
+	};
+	return (
+		<div className="flex flex-col p-3 min-h-screen justify-start items-center">
+			<animated.div
+				onClick={handleClick}
+				style={springs}
+				className="cursor-pointer rounded-full flex-initial shadow-xl w-16 h-16 p-3 mb-3  bg-gradient-to-r from-cyan-500 to-blue-500"
+			></animated.div>
 		</div>
-		<div className='flex-initial rounded-full shadow-xl w-16 h-16 p-3 mb-3  bg-gradient-to-r from-cyan-500 to-blue-500'>
-			<Image src={require("../src/img/airplane.png")} className='w-10 h-10'/>
-		</div>
-	</div>
-  )
+	);
 }
