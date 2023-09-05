f575f0282273bf92229c0d0aa75eb1fbb2af1cb6
zune2222 <zun_e@kakao.com>
2023-09-05 19:09:21 +0900

    remove useless package

--- app/log/page.js
+++ app/log/page.js
@@ -2,8 +2,6 @@
 import CardHover from "../components/cardHover";
 import TravelButton from "../components/travelButton";
 import LogPic from "./components/logPic";
-import PageTransition from "../components/pageTransition";
-import { AnimatePresence } from "framer-motion";
 import useObserver from "../components/useObserver";
 import LogTitle from "./components/logTitle";
 import LogBooks from "./components/logBooks";
