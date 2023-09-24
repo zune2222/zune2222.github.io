215b8077749316f82a112ddf38e43024551d44ea
zune2222 <zun_e@kakao.com>
2023-09-24 20:27:33 +0900

    out

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "export",
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = withContentlayer(nextConfig);
