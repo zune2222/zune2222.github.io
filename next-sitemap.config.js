74cbc167eb9fa62daa8fa67e2dd128ddf8dd4a9a
zune2222 <zun_e@kakao.com>
2023-10-21 09:06:07 +0900

    sitemap add

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://zune2222.github.io",
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
+  exclude: [],
+};
