5833430b03187778f1303d3e717922d9dcf033ee
zune2222 <zun_e@kakao.com>
2023-10-21 09:50:28 +0900

    googleAnalytics add

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { appleFontL } from "./components/fontZip";
 import Wrap from "./components/wrap";
 import CopyRightFooter from "./components/copyrightFooter";
+import GoogleAnalytics from "./components/googleAnalytics";
 export const metadata = {
   title: "zun2log",
   description: "Pak Jun Yi's blog",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body className={appleFontL.className}>
+        <GoogleAnalytics />
         <Wrap>{children}</Wrap>
         <CopyRightFooter />
       </body>
