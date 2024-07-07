7877aea4637cd0421398cecadd8c75fb55f2768f
박준이 <zun_e@zunsmac.local>
2024-07-08 08:28:54 +0900

    CLS bug fix

--- app/log/components/logPic.js
+++ app/log/components/logPic.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function LogPic() {
   return (
     <>
-      <div className="flex bg-red-50 flex-row w-full z-1 h-auto">
+      <div className="aspect-w-16 aspect-h-9 flex bg-red-50 flex-row w-full z-1 h-auto">
         <div className="mt-5 flex z-0">
           <Image
             placeholder="blur"
