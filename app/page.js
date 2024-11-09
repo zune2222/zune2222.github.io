37eccceef2f6f3c815c7c7abafc620ffc8387f10
박준이 <zun_e@zunsmac.local>
2024-11-09 22:41:21 +0900

    home able

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 "use client";
 import HomePic from "./components/homePic";
 import HomeIntro from "./components/homeIntro";
-// import CopyRightFooter from "./components/copyrightFooter";
 export default function Home() {
   return (
     <>
-      {/* <HomePic />
-      <HomeIntro /> */}
+      <HomePic />
+      <HomeIntro />
     </>
   );
 }
