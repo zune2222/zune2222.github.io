d41082d383c6a7b9b06a5847b7b793310cf4320b
박준이 <zun_e@zunsmac.local>
2024-11-09 22:42:16 +0900

    travel btn modify

--- app/components/travelButton.js
+++ app/components/travelButton.js
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Link from "next/link";
@@ -14,46 +13,37 @@ import logoTranparency from "../../src/img/logoTranparency.png";
 import daisyBooks from "../../src/img/daisyBooks.png";
 import memory from "../../src/img/memoryParis.png";
 import parisTranparency from "../../src/img/memoryParisTranparency.png";
-// import coin from "../../src/img/coin.png";
+import coin from "../../src/img/coin.png";
 import { usePathname } from "next/navigation";
+
 export default function TravelButton() {
   const [currentMode, setCurrentMode] = useState(false);
   const [currentPagePic, setCurrentPagePic] = useState(logoTranparency);
-
   const pathname = usePathname();
   const springApi = useSpringRef();
+
   useEffect(() => {
-    if (pathname === "/") {
-      setCurrentPagePic(logoTranparency);
-    }
-    if (pathname === "/log") {
-      setCurrentPagePic(daisyBooks);
-    }
-    if (pathname === "/memory") {
-      setCurrentPagePic(parisTranparency);
-    }
+    if (pathname === "/") setCurrentPagePic(logoTranparency);
+    if (pathname === "/log") setCurrentPagePic(daisyBooks);
+    if (pathname === "/memory") setCurrentPagePic(parisTranparency);
   }, [pathname]);
+
   const springs = useSpring(
     currentMode === false
       ? {
           ref: springApi,
-          config: {
-            tension: 300,
-            mass: 0.1,
-          },
+          config: { tension: 300, mass: 0.1 },
           width: "4.3rem",
           height: "4.3rem",
         }
       : {
           ref: springApi,
-          config: {
-            tension: 250,
-            mass: 2,
-          },
+          config: { tension: 250, mass: 2 },
           width: "20rem",
           height: "8rem",
         }
   );
+
   const data = [
     { link: "/", src: logo, class: "w-12 h-12 rounded-full", key: 1 },
     {
@@ -68,7 +58,14 @@ export default function TravelButton() {
       class: "w-12 h-12 rounded-full bg-red-50",
       key: 3,
     },
+    {
+      link: "https://python-bitcoin-auto-trading.web.app/",
+      src: coin,
+      class: "w-12 h-12 rounded-full bg-red-50",
+      key: 4,
+    },
   ];
+
   const transApi = useSpringRef();
   const transitions = useTransition(
     currentMode ? data : [{ src: currentPagePic, class: "w-12 h-12" }],
@@ -81,10 +78,17 @@ export default function TravelButton() {
       reset: true,
     }
   );
+
   useChain([springApi, transApi], [0, 0.2]);
+
   const handleClick = () => {
     setCurrentMode(!currentMode);
   };
+
+  const handleExternalLinkClick = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <div className="z-50 fixed w-full top-0">
       <div className="z-100 flex flex-col p-3 justify-start items-center">
@@ -103,14 +107,25 @@ export default function TravelButton() {
             {transitions((style, item) => (
               <animated.div style={{ ...style }}>
                 {currentMode ? (
-                  <Link href={`${item.link}`}>
-                    <Image
-                      placeholder="blur"
-                      alt="travleButtonImage"
-                      src={item.src}
-                      className={item.class}
-                    />
-                  </Link>
+                  item.link.startsWith("http") ? (
+                    <div onClick={() => handleExternalLinkClick(item.link)}>
+                      <Image
+                        placeholder="blur"
+                        alt="travelButtonImage"
+                        src={item.src}
+                        className={item.class}
+                      />
+                    </div>
+                  ) : (
+                    <Link href={item.link}>
+                      <Image
+                        placeholder="blur"
+                        alt="travelButtonImage"
+                        src={item.src}
+                        className={item.class}
+                      />
+                    </Link>
+                  )
                 ) : (
                   <Image
                     placeholder="blur"
